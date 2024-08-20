063be4c14c6f544b597ad3faa2bbac5c6ec4e3f7
krisz3001 <horvath.krisztian3001@gmail.com>
2024-08-20 02:17:29 +0200

    changed region of functions to eu

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,31 +9,37 @@ export const db = admin.firestore();
 // Cloud Functions
 
 // Clients
-export const handleClientCreate = functions.firestore.document('clients/{clientId}').onCreate(async (snap, context) => {
-  await db.runTransaction(async (transaction) => {
-    transaction.update(snap.ref, {
-      firebaseId: snap.id,
-      createdAt: snap.createTime,
-      updatedAt: snap.createTime,
+export const handleClientCreate = functions
+  .region('europe-west1')
+  .firestore.document('clients/{clientId}')
+  .onCreate(async (snap, context) => {
+    await db.runTransaction(async (transaction) => {
+      transaction.update(snap.ref, {
+        firebaseId: snap.id,
+        createdAt: snap.createTime,
+        updatedAt: snap.createTime,
+      });
     });
   });
-});
 
-export const handleClientUpdate = functions.firestore.document('clients/{clientId}').onUpdate(handleDocumentUpdate);
+export const handleClientUpdate = functions.region('europe-west1').firestore.document('clients/{clientId}').onUpdate(handleDocumentUpdate);
 
 // Offers
-export const handleOfferCreate = functions.firestore.document('offers/{offerId}').onCreate(createOffer);
-export const handleOfferUpdate = functions.firestore.document('offers/{offerId}').onUpdate(handleDocumentUpdate);
+export const handleOfferCreate = functions.region('europe-west1').firestore.document('offers/{offerId}').onCreate(createOffer);
+export const handleOfferUpdate = functions.region('europe-west1').firestore.document('offers/{offerId}').onUpdate(handleDocumentUpdate);
 
 // Experts
-export const onUserRegister = functions.auth.user().onCreate(async (user, context) => {
-  await db.collection('experts').doc(user.uid).set(
-    {
-      email: user.email,
-      firebaseId: user.uid,
-      createdAt: context.timestamp,
-      updatedAt: context.timestamp,
-    },
-    { merge: true },
-  );
-});
+export const onUserRegister = functions
+  .region('europe-west1')
+  .auth.user()
+  .onCreate(async (user, context) => {
+    await db.collection('experts').doc(user.uid).set(
+      {
+        email: user.email,
+        firebaseId: user.uid,
+        createdAt: context.timestamp,
+        updatedAt: context.timestamp,
+      },
+      { merge: true },
+    );
+  });
